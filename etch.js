2ef1a5faae7b523ee9c59292b6c943cb7d56edc3
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2023-10-02 16:41:25 -0400

    fix functionality for the reload button to clear grid without refreshing page

--- etch.js
+++ etch.js
@@ -1,20 +1,17 @@
 const gridContainer = document.querySelector('#gridContainer');
 const plumColor = document.querySelector('#plum');
 const defaultColor = document.querySelector('#default');
+const clearGrid = document.querySelector('#reload');
 let backgroundColor = "square-background"
+let gridSize = "24";
 
-function createGrid(){
-   //type = "square-background";
-    /*if(color === "plum"){
-        type = "square-background-plum";
-    } else type = "square-background";*/
-    
-    for(let i = 0; i < 24; i++){
-        let gridColumn = document.createElement('div');
+function createGrid(size){    
+    for(let i = 0; i < size; i++){
+        const gridColumn = document.createElement('div');
         gridColumn.classList.add('grid-column');
         gridContainer.appendChild(gridColumn); 
-        for(let j = 0; j < 24; j++){
-            let gridSquare = document.createElement('div');
+        for(let j = 0; j < size; j++){
+            const gridSquare = document.createElement('div');
             gridSquare.setAttribute('id', 'gridSquare');
             gridColumn.appendChild(gridSquare);
         } 
@@ -32,12 +29,15 @@ plumColor.addEventListener('click', () => {
     backgroundColor = "square-background-plum";
 });
 
+clearGrid.addEventListener('click', () => location.reload());
+
+function erase(){
+    createGrid(gridSize);
+}
+
+createGrid(gridSize);
 
 
-createGrid();
-
-//const defaultColor = document.querySelector('#default');
-//defaultColor.addEventListener('click', type = "square-background");
 
 
 
