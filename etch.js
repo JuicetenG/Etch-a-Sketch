61ea2d35c12144a56badbc4b3a876a75b950793b
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2023-10-04 15:49:36 -0400

    add slider to index and add logic for slider to change grid size

--- etch.js
+++ etch.js
@@ -2,15 +2,16 @@ const gridContainer = document.querySelector('#gridContainer');
 const plumColor = document.querySelector('#plum');
 const defaultColor = document.querySelector('#default');
 const clearGrid = document.querySelector('#reload');
+const sliderInput = document.querySelector('#slider');
 let backgroundColor = "square-background"
-let gridSize = "24";
+let gridSize = sliderInput.value;
 
-function createGrid(size){    
-    for(let i = 0; i < size; i++){
+function createGrid(){    
+    for(let i = 0; i < gridSize; i++){
         const gridColumn = document.createElement('div');
         gridColumn.classList.add('grid-column');
         gridContainer.appendChild(gridColumn); 
-        for(let j = 0; j < size; j++){
+        for(let j = 0; j < gridSize; j++){
             const gridSquare = document.createElement('div');
             gridSquare.setAttribute('id', 'gridSquare');
             gridColumn.appendChild(gridSquare);
@@ -18,8 +19,26 @@ function createGrid(size){
     }
 
     const grid = document.querySelectorAll('#gridSquare');
-    grid.forEach(square => square.addEventListener('mouseover', () => square.classList.add(backgroundColor)));
+    grid.forEach(square => square.addEventListener('mouseover', changeColor));
+    clearGrid.addEventListener('click', eraseGrid);
+
+    function eraseGrid(){
+        grid.forEach(square => square.removeAttribute('class'));
+    }
 }
+
+function removeGrid(){
+    gridSize = sliderInput.value;
+    let gridSquares = gridContainer.querySelectorAll('div');
+    gridSquares.forEach(square => square.remove());
+    createGrid()
+}
+
+function changeColor(e){
+    e.target.classList.add(backgroundColor);
+}
+
+sliderInput.addEventListener('input', removeGrid);
 
 defaultColor.addEventListener('click', () => {
     backgroundColor = "square-background";
@@ -29,13 +48,7 @@ plumColor.addEventListener('click', () => {
     backgroundColor = "square-background-plum";
 });
 
-clearGrid.addEventListener('click', () => location.reload());
-
-function erase(){
-    createGrid(gridSize);
-}
-
-createGrid(gridSize);
+createGrid();
 
 
 
